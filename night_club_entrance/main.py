57d19bb7bef0b6caa4e623be45e5439329b29eff
Sami Jaghouar <sami.jaghouar@jina.ai>
2022-04-07 16:32:44 +0200

    feat: initial commit

---
+++ night_club_entrance/main.py
@@ -0,0 +1,20 @@
+import night_club_entrance.helper
+
+
+class Bouncer:
+    def should_i_kick_him_out(self, age: int) -> bool:
+
+        if age < 18:
+            return True
+        else:
+            return False
+
+
+if __name__ == '__main__':
+
+    bouncer = Bouncer()
+
+    if bouncer.should_i_kick_him_out(age=17):
+        print('why are u even trying to get in ??')
+    else:
+        print('let him in')
---
+++ tests/test_night_club_entrance.py
@@ -0,0 +1,5 @@
+from night_club_entrance import __version__
+
+
+def test_version():
+    assert __version__ == '0.1.0'
